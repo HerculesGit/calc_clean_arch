b6cd77a9690e969cdc25c6dca418afe72aec6a61
Hercules <hercules.silva@dcx.ufpb.br>
2022-09-06 20:12:14 -0300

    move AdditionalUseCase from test to domain layer

---
+++ lib/domain/usecases/addition_use_case.dart
@@ -0,0 +1,3 @@
+class AdditionUseCase {
+  int call({required int a, required int b}) => a + b;
+}
--- test/domain/usecases/addition_usecase_test.dart
+++ test/domain/usecases/addition_usecase_test.dart
@@ -1,32 +1,29 @@
+import 'package:calc_clean_arch/domain/usecases/addition_use_case.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-class AdditionUseCase {
-  int call({required int a, required int b}) => a + b;
-}
 
 void main() {
   group('Addition use case', () {
     test('should sum a + b and returns a positive value', () {
       final sut = AdditionUseCase();
-      final result = sut.call(a: 1, b: 2);
+      final result = sut(a: 1, b: 2);
       expect(result, 3);
     });
 
     test('should sum a + -b and returns a negative value', () {
       final sut = AdditionUseCase();
-      final result = sut.call(a: 1, b: -2);
+      final result = sut(a: 1, b: -2);
       expect(result, -1);
     });
 
     test('should sum 0 + 0 and returns 0 value', () {
       final sut = AdditionUseCase();
-      final result = sut.call(a: 0, b: 0);
+      final result = sut(a: 0, b: 0);
       expect(result, 0);
     });
 
     test('should sum -1 + -1 and returns -4 value', () {
       final sut = AdditionUseCase();
-      final result = sut.call(a: -2, b: -2);
+      final result = sut(a: -2, b: -2);
       expect(result, -4);
     });
   });
