122a5a96e6df79ff4f511ae8c561ed8c3b52fa71
Hercules <hercules.silva@dcx.ufpb.br>
2022-09-06 21:36:42 -0300

    remove HistoryEntity instance from GetHistoryUseCase test

--- test/domain/usecases/get_histories_usecase_test.dart
+++ test/domain/usecases/get_histories_usecase_test.dart
@@ -1,7 +1,5 @@
-import 'package:calc_clean_arch/domain/entity/history.dart';
 import 'package:calc_clean_arch/domain/repositories/history_repository.dart';
 import 'package:calc_clean_arch/domain/usecases/get_histories_usecase.dart';
-import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -12,11 +10,9 @@ void main() {
     test('should call getHistories and returns a list of the Historic',
         () async {
       final historyRepositorySpy = HistoryRepositorySpy();
-      String text = faker.randomGenerator.string(2);
 
-      final historyEntity = HistoryEntity(text);
       final sut = GetHistoriesUseCase(historyRepositorySpy);
-      await sut(params: historyEntity);
+      await sut(params: null);
 
       verify(historyRepositorySpy.getHistories()).called(1);
       verifyNoMoreInteractions(historyRepositorySpy);
