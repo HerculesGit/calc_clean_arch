aa75745104798260b42e22a25c36be86b3877bb6
Hercules <hercules.silva@dcx.ufpb.br>
2022-09-09 01:11:17 -0300

    adjust keyboard widget to a responsive layout

--- lib/presentation/widgets/calculator_keyboard_widget.dart
+++ lib/presentation/widgets/calculator_keyboard_widget.dart
@@ -10,49 +10,57 @@ class CalculatorKeyboardWidget extends StatelessWidget {
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) => _buildKeyboard(context);
+  Widget build(BuildContext context) => Container(
+        padding: const EdgeInsets.only(bottom: kDefaultMargin + 20),
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25),
+              topLeft: Radius.circular(25),
+            ),
+            color: Color(0xFFEEEEEE)),
+        child: Column(children: _buildCustomGridView()),
+      );
 
-  Widget _buildKeyboard(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: kDefaultMargin + 20),
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(25),
-          topLeft: Radius.circular(25),
-        ),
-        color: Color(0xFFEEEEEE),
-      ),
-      child: GridView.count(
-        physics: const NeverScrollableScrollPhysics(),
-        crossAxisCount: 4,
-        shrinkWrap: true,
-        addAutomaticKeepAlives: false,
-        childAspectRatio: 2 / 1.9,
-        padding: const EdgeInsets.only(top: 32.0, bottom: kDefaultMargin),
-        children: keyboards
-            .map((keyboard) => _buildKeyboardButton(keyboard))
-            .toList(),
-      ),
-    );
+  List<Widget> _buildCustomGridView() {
+    const crossAxisCount = 4;
+    final List<Widget> crossAxisChildren = [];
+    final List<Widget> mainAxisChildren = [];
+
+    for (var keyboard in keyboards) {
+      if (crossAxisChildren.length == crossAxisCount) {
+        mainAxisChildren
+            .add(Expanded(child: Row(children: List.from(crossAxisChildren))));
+        crossAxisChildren.clear();
+      }
+      crossAxisChildren.add(_buildKeyboardButton(keyboard));
+    }
+
+    mainAxisChildren
+        .add(Expanded(child: Row(children: List.from(crossAxisChildren))));
+
+    return mainAxisChildren;
   }
 
   Widget _buildKeyboardButton(KeyboardViewModel keyboardViewModel) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: kDefaultMargin / 2),
-      child: Container(
-        margin: const EdgeInsets.all(4.0),
-        color: Colors.red,
-        child: GestureDetector(
-          onTap: () => keyboardViewModel.enabled
-              ? onButtonTapped(keyboardViewModel)
-              : () {},
-          child: Text(
-            keyboardViewModel.key,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: keyboardViewModel.lightColor,
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => keyboardViewModel.enabled
+            ? onButtonTapped(keyboardViewModel)
+            : () {},
+        child: AbsorbPointer(
+          absorbing: true,
+          child: Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(4.0),
+            // color: Colors.red,
+            child: Text(
+              keyboardViewModel.key,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: keyboardViewModel.lightColor,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
